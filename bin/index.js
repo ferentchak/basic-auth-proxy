b9f896e88ac636287da1d0d256bee7f2a611f1e2
Charles <Ferentchak@gmail.com>
2013-01-20 15:29:41 -0500

    better support for usage as a node module

--- bin/index.js
+++
@@ -1,9 +0,0 @@
-require("coffee-script");
-fs = require("fs")
-proxy = require("./../lib/proxy");
-path = require("path");
-currentDirectory = process.cwd();
-
-var configFileLocation = path.join(currentDirectory, "config.json");
-var config = JSON.parse(fs.readFileSync(configFileLocation));
-proxy(config);
